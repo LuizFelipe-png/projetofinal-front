19112c61aa36e8902262809cee1fc8ad8b65d271
LuizFelipe-png <luizfelipemurarolli@gmail.com>
2026-07-31 17:28:54 -0300

    projeto enviando email com sucesso, falta terminar a tela de entregador e fazer o rastreamento

--- src/main/java/com/main/front/TCC_front/model/IncidentesDTO.java
+++ src/main/java/com/main/front/TCC_front/model/IncidentesDTO.java
@@ -1,95 +1,95 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.main.front.TCC_front.model;
+    /*
+     * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+     * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+     */
+    package com.main.front.TCC_front.model;
 
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+    import java.sql.Date;
+    import java.sql.Timestamp;
+    import java.time.LocalDateTime;
 
-/**
- *
- * @author Win
- */
-public class IncidentesDTO {
+    /**
+     *
+     * @author Win
+     */
+    public class IncidentesDTO {
 
-    private int id_incidente;
-    private Integer id_pedido;
-    private String tipo;
-    private String descricao;
-    private String acao_tomada;
-    private LocalDateTime data_ocorrencia;
-    private String codigo_pedido;
+        private int id_incidente;
+        private Integer id_pedido;
+        private String tipo;
+        private String descricao;
+        private String acao_tomada;
+        private LocalDateTime data_ocorrencia;
+        private String codigo_pedido;
 
-    public IncidentesDTO() {
+        public IncidentesDTO() {
+        }
+
+        public IncidentesDTO(int id_incidente, Integer id_pedido, String tipo, String descricao, String acao_tomada, LocalDateTime data_ocorrencia, String codigo_pedido) {
+            this.id_incidente = id_incidente;
+            this.id_pedido = id_pedido;
+            this.tipo = tipo;
+            this.descricao = descricao;
+            this.acao_tomada = acao_tomada;
+            this.data_ocorrencia = data_ocorrencia;
+            this.codigo_pedido = codigo_pedido;
+        }
+
+        public int getId_incidente() {
+            return id_incidente;
+        }
+
+        public void setId_incidente(int id_incidente) {
+            this.id_incidente = id_incidente;
+        }
+
+        public Integer getId_pedido() {
+            return id_pedido;
+        }
+
+        public void setId_pedido(Integer id_pedido) {
+            this.id_pedido = id_pedido;
+        }
+
+        public String getTipo() {
+            return tipo;
+        }
+
+        public void setTipo(String tipo) {
+            this.tipo = tipo;
+        }
+
+        public String getDescricao() {
+            return descricao;
+        }
+
+        public void setDescricao(String descricao) {
+            this.descricao = descricao;
+        }
+
+        public String getAcao_tomada() {
+            return acao_tomada;
+        }
+
+        public void setAcao_tomada(String acao_tomada) {
+            this.acao_tomada = acao_tomada;
+        }
+
+        public LocalDateTime getData_ocorrencia() {
+            return data_ocorrencia;
+        }
+
+        public void setData_ocorrencia(LocalDateTime data_ocorrencia) {
+            this.data_ocorrencia = data_ocorrencia;
+        }
+
+        public String getCodigo_pedido() {
+            return codigo_pedido;
+        }
+
+        public void setCodigo_pedido(String codigo_pedido) {
+            this.codigo_pedido = codigo_pedido;
+        }
+
+
     }
-
-    public IncidentesDTO(int id_incidente, Integer id_pedido, String tipo, String descricao, String acao_tomada, LocalDateTime data_ocorrencia, String codigo_pedido) {
-        this.id_incidente = id_incidente;
-        this.id_pedido = id_pedido;
-        this.tipo = tipo;
-        this.descricao = descricao;
-        this.acao_tomada = acao_tomada;
-        this.data_ocorrencia = data_ocorrencia;
-        this.codigo_pedido = codigo_pedido;
-    }
-
-    public int getId_incidente() {
-        return id_incidente;
-    }
-
-    public void setId_incidente(int id_incidente) {
-        this.id_incidente = id_incidente;
-    }
-
-    public Integer getId_pedido() {
-        return id_pedido;
-    }
-
-    public void setId_pedido(Integer id_pedido) {
-        this.id_pedido = id_pedido;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getAcao_tomada() {
-        return acao_tomada;
-    }
-
-    public void setAcao_tomada(String acao_tomada) {
-        this.acao_tomada = acao_tomada;
-    }
-
-    public LocalDateTime getData_ocorrencia() {
-        return data_ocorrencia;
-    }
-
-    public void setData_ocorrencia(LocalDateTime data_ocorrencia) {
-        this.data_ocorrencia = data_ocorrencia;
-    }
-
-    public String getCodigo_pedido() {
-        return codigo_pedido;
-    }
-
-    public void setCodigo_pedido(String codigo_pedido) {
-        this.codigo_pedido = codigo_pedido;
-    }
-
-    
-}
