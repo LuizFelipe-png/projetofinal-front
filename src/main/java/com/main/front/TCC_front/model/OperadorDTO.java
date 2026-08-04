da831a8dd85da581e8cb0c400921f8440773bd5f
LuizFelipe-png <luizfelipemurarolli@gmail.com>
2026-08-04 17:28:30 -0300

    falta resolver o novo-lote que esta bugado

--- src/main/java/com/main/front/TCC_front/model/OperadorDTO.java
+++ src/main/java/com/main/front/TCC_front/model/OperadorDTO.java
@@ -15,7 +15,6 @@ public class OperadorDTO {
     private float peso;
     private int quantidade;
     private String status;
-    private String codigo;
     private int id_cliente;
     private String nome_cliente;
     private String email_cliente;
@@ -23,13 +22,12 @@ public class OperadorDTO {
     public OperadorDTO() {
     }
 
-    public OperadorDTO(int id_pedido, String nome_pedido, float peso, int quantidade, String status, String codigo, int id_cliente, String nome_cliente, String email_cliente) {
+    public OperadorDTO(int id_pedido, String nome_pedido, float peso, int quantidade, String status, int id_cliente, String nome_cliente, String email_cliente) {
         this.id_pedido = id_pedido;
         this.nome_pedido = nome_pedido;
         this.peso = peso;
         this.quantidade = quantidade;
         this.status = status;
-        this.codigo = codigo;
         this.id_cliente = id_cliente;
         this.nome_cliente = nome_cliente;
         this.email_cliente = email_cliente;
@@ -38,55 +36,66 @@ public class OperadorDTO {
     public int getId_pedido() {
         return id_pedido;
     }
+
     public void setId_pedido(int id_pedido) {
         this.id_pedido = id_pedido;
     }
+
     public String getNome_pedido() {
         return nome_pedido;
     }
+
     public void setNome_pedido(String nome_pedido) {
         this.nome_pedido = nome_pedido;
     }
+
     public float getPeso() {
         return peso;
     }
+
     public void setPeso(float peso) {
         this.peso = peso;
     }
+
     public int getQuantidade() {
         return quantidade;
     }
+
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
+
     public String getStatus() {
         return status;
     }
+
     public void setStatus(String status) {
         this.status = status;
     }
-    public String getCodigo() {
-        return codigo;
-    }
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
+
     public int getId_cliente() {
         return id_cliente;
     }
+
     public void setId_cliente(int id_cliente) {
         this.id_cliente = id_cliente;
     }
+
     public String getNome_cliente() {
         return nome_cliente;
     }
+
     public void setNome_cliente(String nome_cliente) {
         this.nome_cliente = nome_cliente;
     }
+
     public String getEmail_cliente() {
         return email_cliente;
     }
+
     public void setEmail_cliente(String email_cliente) {
         this.email_cliente = email_cliente;
     }
+
+    
 }
